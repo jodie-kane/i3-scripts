232a17f5c90ff62bfa648cb4371f212603bd384a
JoeKane <joe.kane@frogtoad.co.uk>
2019-08-03 10:52:53 +0200

    additional comments that explain superfluousness of the content of this script ;)

--- i3-window-switcher.sh
+++ i3-window-switcher.sh
@@ -2,6 +2,10 @@
 
 ### hideously inefficient script; but it doesn't need to be ;)
 ### uses rofi
+
+# could actually be: i3-window-apply.sh focus
+# or even just:      i3-window-apply.sh 
+# or technically, deleted in favour of the above ;)
 con_id="$( i3-get-window-id.sh )"
 if [[ -n "${con_id}" ]]
 then 
